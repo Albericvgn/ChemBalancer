33af68dfe0e588a34cc14583c7dfc70409ec5150
faneshala <fane.shala@epfl.ch>
2024-05-26 16:11:03 +0200

    encore une fois

--- tests/test_example_module.py
+++ tests/test_example_module.py
@@ -1,5 +1,19 @@
 
-from src/chembalancer.chembalancer import hello_smiles
+import pytest
+import requests
+from requests.exceptions import HTTPError
+from requests_mock import Mocker
+import sys
+import os
+
+try:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+except NameError:
+    current_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+src_path = os.path.join(current_dir, '..', 'src')
+sys.path.insert(0, src_path)
+
+from chembalancer import test_hello_smiles  
 
 
 # Test the function
