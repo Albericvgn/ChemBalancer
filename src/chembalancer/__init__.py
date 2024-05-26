79fdfcbd390656a9d7749298bb27b650ca0b013a
Albéric <alberic.vigne@epfl.ch>
2024-05-26 13:36:28 +0200

    Initial commit

---
+++ src/chembalancer/__init__.py
@@ -0,0 +1,5 @@
+"""Chemical Equation Balancer."""
+
+from __future__ import annotations
+
+__version__ = "0.0.1"
---
+++ tests/test_example_module.py
@@ -0,0 +1,6 @@
+from chembalancer.example_module import hello_smiles
+
+
+# Test the function
+def test_hello_smiles():
+    assert hello_smiles("C(=O)O") == "Hello, C(=O)O!", "Test failed: SMILES input"
